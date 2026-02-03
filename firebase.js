2d2cd855fd6cf92a6322147847553d34a55d007a
MRNIRAJETHH <missseemaroy221@gmail.com>
2026-02-03 12:20:04 -0500

    Refactor Firebase initialization to check existing apps

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -8,7 +7,6 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
